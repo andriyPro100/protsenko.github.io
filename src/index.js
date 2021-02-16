a017982847f93dd9f079b171d7a8e60696c8b057
andriyPro100 <andriy.anatolich@gmail.com>
2021-02-16 14:56:39 +0200

    add block about

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 80) {
+        $("#menu").addClass("navbar-inner_white");
+    } else {
+        $("#menu").removeClass("navbar-inner_white");
+    }
+});
