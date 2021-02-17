9fc35f58b183c89a29e78c4fe8cda33b01c8c4cd
andriyPro100 <andriy.anatolich@gmail.com>
2021-02-17 09:27:26 +0200

    final version 1.0.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 80) {
+    if ($(document).scrollTop() > 100) {
         $("#menu").addClass("navbar-inner_white");
     } else {
         $("#menu").removeClass("navbar-inner_white");
